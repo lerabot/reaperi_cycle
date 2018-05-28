dbcafab391566524e9e59688b02569dae3fbe16b
lerabot <r.rabot@gmail.com>
2018-05-28 08:37:51 -0400

    added a quad drawing function for testing

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <GL/gl.h>
 #include <GL/glkos.h>
-#include <GL/glu.h>
 #include <time.h>
 #include <dc/pvr.h>
 #include <stdio.h>
@@ -25,17 +24,92 @@ lua_State *L;
 
 texture t;
 
+void draw_quad(texture *tex, float x, float y) {
+	float texW = tex->w * tex->uSize * tex->xScale;
+	float texH = tex->h * tex->vSize * tex->yScale;
+	float x0 = x - texW / 2;
+	float y0 = y - texH / 2;
+	float x1 = x + texW / 2;
+	float y1 = y + texH / 2;
+	float u = tex->u;
+	float v = tex->v;
+	float xS = tex->uSize;
+	float yS = tex->vSize;
+
+	float vertex_data[] = {
+		/* 2D Coordinate, texture coordinate */
+		x0, y1, 0.0f,
+		x0, y0, 0.0f,
+		x1, y1, 0.0f,
+		x1, y0, 0.0f
+	};
+
+	float uv_data[] = {
+		/* 2D Coordinate, texture coordinate */
+		u, v + yS,
+		u, v,
+		u + xS, v + yS,
+		u + xS, v
+	};
+
+	float normal_data[] = {
+		/* 2D Coordinate, texture coordinate */
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0
+	};
+
+	float color_data[] = {
+		/* 2D Coordinate, texture coordinate */
+		1.0, 1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0, 1.0
+	};
+
+	//setString(0, tex->id);
+
+	if (&tex->id != NULL) {
+		//glEnable(GL_TEXTURE_2D);
+		//printf("tex id adress = %p\n", (void *) &tex->id);
+		//glBindTexture(GL_TEXTURE_2D, tex->id);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, tex->min_filter);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, tex->mag_filter);
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		//glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+		glVertexPointer(3, GL_FLOAT, 0, vertex_data);
+		//glTexCoordPointer(2, GL_FLOAT, 0, uv_data);
+		glNormalPointer(GL_FLOAT, 0, normal_data);
+		glColorPointer(4, GL_FLOAT, 0, color_data);
+
+		//glColor4f(tex->light * highlight[0],tex->light * highlight[1],tex->light * highlight[2], tex->a);
+		//glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+		//glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, vertex_data);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+		//glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	}
+}
+
 int main()
 {
   glKosInit();
   enableTrans();
   glMatrixMode(GL_PROJECTION);
-  glOrtho(0, 640, 0, 480, -50, 50);
-  glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  //glShadeModel(GL_SMOOTH);
+  glOrtho(0, 640, 0, 480, -50, 50);
   //glScalef(1.f/320.f, 1.f/240.f, 100.f);
   //glTranslatef(-320, -240, 0);
+  //glOrtho(0, 640, 0, 480, -50, 50);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glEnable(GL_LIGHTING);
+  //glEnable(GL_LIGHT0);
 
   //sounds stuff
   snd_stream_init();
@@ -44,8 +118,6 @@ int main()
 
   setLuaState(&L);
   setLuaState(&t_data);
-
-  //lua_push
 
   p1 = initPlayer(0);
   loadFont("/rd/DFKei.png");
@@ -56,6 +128,8 @@ int main()
   loadTest(currentScene);
 
   png_to_gl_texture(&t, "/rd/DFKei.png");
+  setInt(1, glIsTexture(t.id));
+
 
   uint64_t b, e;
   double avg;
@@ -68,14 +142,15 @@ int main()
       free(tempScene);
       *currentScene = *tempScene;
     }
-
     pvr_get_stats(&stats);
     updatePlayer();
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-    glTranslated((int)displayPos[0], (int)displayPos[1], displayPos[2]);
-    //draw_textured_quad(&t, 320, 240);
-    drawCursor();
+    //glTranslated((int)displayPos[0], (int)displayPos[1], displayPos[2]);
+    draw_quad(&t, 0, 0);
+    //drawCursor();
+
 
     /*
     drawScene(currentScene);
@@ -94,7 +169,6 @@ int main()
 
     //adds the buttons to the previously pressed button
     p1.pstate.buttons &= p1.state->buttons;
-
 
     //frameCount++;
 
